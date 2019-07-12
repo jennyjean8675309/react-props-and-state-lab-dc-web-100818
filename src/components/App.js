84781836ba58e1467044fb1aef76435c01078782
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-07-12 16:11:39 -0400

    completed pet adoption react lab

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,55 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (event) => {
+    let filterType = event.target.value
+    this.setState({
+      ...this.state.pets,
+      ...this.state.filters.type = filterType
+    })
+  }
+
+  onFindPetsClick = ()=> {
+    if (this.state.filters.type === 'all') {
+      fetch('/api/pets')
+      .then(response => response.json())
+      .then(pets => this.setState({
+        pets: pets
+      }))
+    } else {
+      fetch(`/api/pets?type=${this.state.filters.type}`)
+      .then(response => response.json())
+      .then(pets => this.setState({
+        pets: pets
+      }))
+    }
+  }
+
+  // this version of onAdoptPet works, but it appends the updated pet to end of the list no matter where it was originally - not the best user experience
+  // onAdoptPet = (petId) => {
+  //   let updatedPet = this.state.pets.find(pet => pet.id === petId)
+  //   updatedPet.isAdopted = true
+  //   console.log(updatedPet)
+  //   let otherPets = this.state.pets.filter(pet => pet.id !== updatedPet.id)
+  //   otherPets.push(updatedPet)
+  //   this.setState({
+  //     pets: otherPets
+  //   }) 
+  // }
+
+  // this version of onAdoptPet uses .map(), which returns a new array with all of the same elements in the same order, only modified
+  // it also uses the spread operator (...) to destructure the pet object first, and then overwrite the value of the isAdopted key
+  onAdoptPet = (petId) => {
+    console.log(petId)
+    let newPets = this.state.pets.map(pet => {
+      return pet.id === petId ? { ...pet, isAdopted: true } : pet
+    })
+    console.log(newPets)
+    this.setState({
+      pets: newPets
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +73,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,10 +1,20 @@
 import React from 'react'
-
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+  generatePet = () => {
+    return this.props.pets.map( pet => {
+      console.log(pet)
+      return <Pet key={pet.id} pet={pet} onAdoptPet={this.props.onAdoptPet} isAdopted={pet.isAdopted} />
+    })
+  }
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return (
+    <div className="ui cards">
+      {this.generatePet()}
+    </div>
+    )
   }
 }
 
